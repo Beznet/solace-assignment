8e6dfa45c9c75db1876bd452f4ae32782a75eb5c
Bennett Dungan <bennettdungan@gmail.com>
2025-01-28 14:35:51 -0500

    improved API call for fetch advocates, added error handling with try/catch

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,18 @@
+import { NextResponse } from "next/server";
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
 
 export async function GET() {
-  // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
+  try {
+    const data = await db.select().from(advocates);
 
-  const data = advocateData;
+    return NextResponse.json({ data }, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching advocates:", error);
 
-  return Response.json({ data });
+    return NextResponse.json(
+      { error: "Failed to fetch advocates" },
+      { status: 500 }
+    );
+  }
 }
