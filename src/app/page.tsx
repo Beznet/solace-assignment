20554758c6fd04bfb09448543c14b23d2b4bbe33
Bennett Dungan <bennettdungan@gmail.com>
2025-01-28 13:52:07 -0500

    basic table styling, update function names, add types to Advocate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,18 @@
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [advocates, setAdvocates] = useState([]);
-  const [filteredAdvocates, setFilteredAdvocates] = useState([]);
+  interface Advocate {
+    id: number;
+    firstName: string;
+    lastName: string;
+    city: string;
+    degree: string;
+    specialties: string[];
+    yearsOfExperience: number;
+    phoneNumber: string;
+  }
+  const [advocates, setAdvocates] = useState<Advocate[]>([]);
+  const [filteredAdvocates, setFilteredAdvocates] = useState<Advocate[]>([]);
 
   useEffect(() => {
     console.log("fetching advocates...");
@@ -16,12 +26,11 @@ export default function Home() {
     });
   }, []);
 
-  const onChange = (e) => {
+  const searchAdvocates = (e) => {
     const searchTerm = e.target.value;
 
     document.getElementById("search-term").innerHTML = searchTerm;
 
-    console.log("filtering advocates...");
     const filteredAdvocates = advocates.filter((advocate) => {
       return (
         advocate.firstName.includes(searchTerm) ||
@@ -36,54 +45,84 @@ export default function Home() {
     setFilteredAdvocates(filteredAdvocates);
   };
 
-  const onClick = () => {
+  const resetSearch = () => {
     console.log(advocates);
     setFilteredAdvocates(advocates);
   };
 
   return (
-    <main style={{ margin: "24px" }}>
-      <h1>Solace Advocates</h1>
-      <br />
-      <br />
-      <div>
-        <p>Search</p>
-        <p>
-          Searching for: <span id="search-term"></span>
-        </p>
-        <input style={{ border: "1px solid black" }} onChange={onChange} />
-        <button onClick={onClick}>Reset Search</button>
+    <main className="flex flex-col m-6 p-6">
+      <h1 className="text-3xl font-bold text-center mb-6">Solace Advocates</h1>
+      <div className="mb-6">
+        <div className="flex items-center gap-4 mt-2">
+          <input
+            className="border border-gray-300 rounded-md p-2"
+            placeholder="Search advocates..."
+            onChange={searchAdvocates}
+          />
+          <button
+            className="bg-blue-500 text-white px-4 py-2"
+            onClick={resetSearch}
+          >
+            Reset
+          </button>
+        </div>
       </div>
-      <br />
-      <br />
-      <table>
-        <thead>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>City</th>
-          <th>Degree</th>
-          <th>Specialties</th>
-          <th>Years of Experience</th>
-          <th>Phone Number</th>
+      <table className="table-auto w-full border border-gray-300 rounded-md shadow-md">
+        <thead className="bg-gray-200">
+          <tr>
+            <th className="px-4 py-2 text-center border border-gray-300">
+              First Name
+            </th>
+            <th className="px-4 py-2 text-center border border-gray-300">
+              Last Name
+            </th>
+            <th className="px-4 py-2 text-center border border-gray-300">
+              City
+            </th>
+            <th className="px-4 py-2 text-center border border-gray-300">
+              Degree
+            </th>
+            <th className="px-4 py-2 text-center border border-gray-300">
+              Specialties
+            </th>
+            <th className="px-4 py-2 text-center border border-gray-300">
+              Years of Experience
+            </th>
+            <th className="px-4 py-2 text-center border border-gray-300">
+              Phone Number
+            </th>
+          </tr>
         </thead>
         <tbody>
-          {filteredAdvocates.map((advocate) => {
-            return (
-              <tr>
-                <td>{advocate.firstName}</td>
-                <td>{advocate.lastName}</td>
-                <td>{advocate.city}</td>
-                <td>{advocate.degree}</td>
-                <td>
-                  {advocate.specialties.map((s) => (
-                    <div>{s}</div>
-                  ))}
-                </td>
-                <td>{advocate.yearsOfExperience}</td>
-                <td>{advocate.phoneNumber}</td>
-              </tr>
-            );
-          })}
+          {filteredAdvocates.map((advocate, index) => (
+            <tr key={advocate.id}>
+              <td className="px-4 py-2 text-center border border-gray-300">
+                {advocate.firstName}
+              </td>
+              <td className="px-4 py-2 text-center border border-gray-300">
+                {advocate.lastName}
+              </td>
+              <td className="px-4 py-2 text-center border border-gray-300">
+                {advocate.city}
+              </td>
+              <td className="px-4 py-2 text-center border border-gray-300">
+                {advocate.degree}
+              </td>
+              <td className="px-4 py-2 text-center border border-gray-300">
+                {advocate.specialties.map((s, idx) => (
+                  <div key={idx}>{s}</div>
+                ))}
+              </td>
+
+              <td className="px-4 py-2 text-center border border-gray-300">
+                {advocate.yearsOfExperience}
+              </td>
+              <td className="px-4 py-2 text-center border border-gray-300">
+                {advocate.phoneNumber}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </main>
